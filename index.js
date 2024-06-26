1439e03ffbef3aff13c02ebe8a0d185025d9592a
sahilsaini <sahilsaini7357@gmail.com>
2024-06-26 15:36:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,84 @@
+//string
 let fullName = "sahil saini"
+console.log(fullName)
+
+let name = 'rahul saini'
+console.log(name)
+
+let templeteLiteralStr = `Hello, ${fullName}!`;
+console.log(templeteLiteralStr)
+
+let escapeExample = "He said,\"Hello!\"";
+console.log(escapeExample)
+
+let firstName = "Shankar"
+let lastName = " lal kumar"
+let full = firstName + "" + lastName;
+console.log(full);
+
+//number
+let integerNamber = 10;
+console.log(integerNamber);
+
+let flotingPointNamber = 2.25;
+console.log(flotingPointNamber)
+
+//Boolean 
+
+let isJavaScriptFun = true;
+let isTired = false;
+
+console.log(isJavaScriptFun);
+console.log(isTired);
+
+//Undefiend
+
+let y;
+console.log(y)
+
+let r = null;
+console.log(r);
+
+//symbol
+
+let symbol1 = Symbol();
+let symbol2 = Symbol();
+
+console.log(symbol1 === symbol2); // yeh false dikhayega kyunki har symbol unique hota hai
+
+//bigint
+
+let bigInt1 = 1234567890123456789012345678901234567890n;
+let bigInt2 = 9876543210987654321098765432109876543210n;
+
+let sum = bigInt1 + bigInt2;
+console.log(sum); // Addition
+
+let difference = bigInt2 - bigInt1;
+console.log(difference); // Subtraction
+
+let product = bigInt1 * bigInt2;
+console.log(product); // Multiplication
+
+let quotient = bigInt2 / bigInt1;
+console.log(quotient); // Division, result truncated towards zero
+
+//object
+
+let preson = {
+
+    nam
+}
 
 
-console.log(typeof fullName)
 
+
+//let globle
+
+let x=300;
+
+{
+    document.write(x)
+
+}
 
